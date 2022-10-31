c7e9626717ef1af1337f63d96dec2e443c244e0e
Hayden Schroeder <hschroeder0317@gmail.com>
2022-10-31 17:15:52 -0500

    Working on linearizing data. Also added fix for data clobbered by bad PE3 settings

--- datanalytics.py
+++ datanalytics.py
@@ -36,16 +36,46 @@ def linearize(df):
     if 'Anlg #1' in df.columns:
         df['Anlg #1'] = df['Anlg #1'] * 20
         df.rename(columns={'Anlg #1': 'Oil Pressure (psi)'}, inplace=True)
+
+    # Linearize Anlg #2 and rename to -X Acceleration (g)
+    if 'Anlg #2' in df.columns:
+        df['Anlg #2'] = (df['Anlg #2'] * 2.5) - 6.25
+        df.rename(columns={'Anlg #2': '-X Acceleration (g)'}, inplace=True)
+    
+    # Calculate change in temperature from front radiator (Anlg #3) to back (Anlg #4) Inlet/Outlet Radiator temp
+    if 'Anlg #3' in df.columns and 'Anlg #4' in df.columns:
+        df['Anlg #3'] = df['Anlg #3'] - df['Anlg #4']
+        df.rename(columns={'Anlg #3': 'Radiator DeltaT (F)'}, inplace=True)
+        df = df.drop('Anlg #4', axis=1)
+
+    # Linearize Anlg #6 and rename to O2 (lambda)
+    if 'Anlg #6' in df.columns:
+        df['Anlg #6'] = (df['Anlg #6'] * 0.14) + 0.58
+        df.rename(columns={'Anlg #6': 'O2 (lambda)'}, inplace=True)
+
     return df
 
-def plot(df, x, columns):
+def fixbad(df, columns):
+    for column in columns:
+        if column in df.columns:
+            df[column] = df[column] / 1.954
+    return df
+
+
+def plot(df, x, columns, title=None):
+    for column in columns:
+        if (not column in df.columns):
+            print("Column", column, "not found in data")
+            return
     df.plot(x=x, y=columns)
+    plt.title(title)
     plt.show()
 
 for filename in os.listdir(DIRECTORY):
     print("Processing", filename)
     df = pd.read_csv(f"{DIRECTORY}{filename}", encoding="latin1")
     df = clean(df)
+    df = fixbad(df, ['Anlg #1', 'Anlg #2', 'Anlg #3', 'Anlg #4', 'Anlg #5','Anlg #6','Anlg #7','Anlg #8'])
     df = linearize(df)
-    print(f"{df.describe().transpose()}\n\n")
-    plot(df,'RPM' , ['Oil Pressure (psi)'])
+    # print(f"{df.describe().transpose()}\n\n")
+    # plot(df,'Time (sec)' , ['Anlg #1','Anlg #2','Anlg #3','Anlg #4','Anlg #5','Anlg #6','Anlg #7','Anlg #8'], title=filename)
